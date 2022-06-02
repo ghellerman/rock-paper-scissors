2c8b9aed52205eb9aa4b748f031f6b0a2fa5e4b7
ghellerman <ghellerman@live.ca>
2022-06-02 12:23:32 -0400

    .

--- script.js
+++ script.js
@@ -1,68 +1,126 @@
+        // Get necessary nodes from the DOM tree
+        const images = Array.from(document.querySelectorAll(".card-image"));
+        const results = document.querySelector(".results");
+        const playerScore = document.querySelector(".player-score");
+        const computerScore = document.querySelector(".computer-score");
+        const playerSelection = document.querySelector(".player-selection");
+        const computerSelection = document.querySelector(".computer-selection");
+
+        // Create initial variables
         const myArray = ["rock", "paper", "scissors"];
         let playerPoints = 0;
         let computerPoints = 0;
-        let maxPoints = 5;
+
+        // Start Game when user clicks on an image
+        images.forEach((image) =>
+            image.addEventListener("click", () => {
+                if (playerPoints >= 5 || computerPoints >= 5) {
+                    return;
+                }
+                // console.log(image.dataset.image);
+                game(image.dataset.image);
+            })
+        );
+        
+        // Game Logic
 
         function computerSelect() {
             return myArray[~~(Math.random() * myArray.length)];
         }
 
-        function playerSelect() {
-            let playerInput = prompt("Please input your selection:");
-            let playerSelectionTrim = playerInput.trim();
-            let playerSelect = playerSelectionTrim.toLowerCase();
-            return playerSelect; 
-        }
-
         function playRound(playerSelection, computerSelection) {
             let winner = "";
             if (playerSelection === computerSelection) {
-                winner = "tie";
+                winner = "Tie";
             } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-                winner = "player";
+                winner = "Player";
             } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-                winner = "player";
+                winner = "Player";
             } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-                winner = "player";
+                winner = "Player";
             } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-                winner = "computer";
+                winner = "Computer";
             } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-                winner = "computer";
+                winner = "Computer";
             } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-                winner = "computer";
+                winner = "Computer";
             }
             return winner;
         }
 
+        // function playRound(playerSelection, computerSelection) {
+        //     let winner = '';
+          
+        //     if (playerSelection === 'Rock') {
+        //       if (computerSelection === 'Paper') {
+        //         winner = 'You Lose! Paper beats Rock';
+        //       } else if (computerSelection === 'Scissors') {
+        //         winner = 'You Win! Rock beats Scissors';
+        //       } else {
+        //         winner = "It's a tie";
+        //       }
+        //     } else if (playerSelection === 'Paper') {
+        //       if (computerSelection === 'Scissors') {
+        //         winner = 'You Lose! Scissors beats Paper';
+        //       } else if (computerSelection === 'Rock') {
+        //         winner = 'You Win! Paper beats Rock';
+        //       } else {
+        //         winner = "It's a tie";
+        //       }
+        //     } else if (playerSelection === 'Scissors') {
+        //       if (computerSelection === 'Rock') {
+        //         winner = 'You Lose! Rock beats Scissors';
+        //       } else if (computerSelection === 'Paper') {
+        //         winner = 'You Win! Scissors beats Paper';
+        //       } else {
+        //         winner = "It's a tie";
+        //       }
+        //     }
+          
+        //     return winner;
+        //   }
 
-        function game() {
-            // let maxPoints = prompt("Please enter the number of points to win:");
-            while  (playerPoints < maxPoints && computerPoints < maxPoints) {
-                while (true) {
-                let playerSelection = playerSelect();
+
+
+
+        // function editText(text) {
+        //     const p = document.createElement("p");
+        //     p.textContent = text;
+
+        //     return p;
+        // }
+
+        function game(playerSelection) {
+                // let playerSelection = playerSelect();
                 let computerSelection = computerSelect();
-                let winner = playRound(playerSelection, computerSelection);                   
-                if (playerSelection != "rock" && playerSelection != "paper" && playerSelection != "scissors") {
-                   console.log("sorry, that is not a valid selection");
-                }
-                if (winner == 'computer') {
+                let winner = playRound(playerSelection, computerSelection); 
+
+                // console.log("Player: " + playerSelection);
+                // console.log("Computer: " + computerSelection)
+
+                if (winner.search('Computer') > -1) {
                     computerPoints++;
-                } else if (winner == 'player') {
+                } else if (winner.search("Player") > -1) {
                     playerPoints++;
                 }
-                console.log("Player: " + playerSelection);
-                console.log("Computer: " + computerSelection);
-                console.log( "The winner is: " + winner);
-                console.log("Points: Player = " + playerPoints + ", Computer: " + computerPoints);
-                
-            }
+                console.log(winner);
 
-            //Win condition: 5 points
-            if (playerPoints == maxPoints) {
-                console.log (" Congratulations, you win!");
-            } else if (computerPoints == maxPoints) {
-                console.log ("The computer wins, better luck next time!");
+                playerScore.textContent = "Player: " + playerPoints;
+                computerScore.textContent = "Computer: " + computerPoints;
+                results.textContent = "Winner: " + winner;
+
+                if (playerPoints >= 5) {
+                    alert(" Congratulations, you win!");
+                    playerPoints = 0;
+                    computerPoints = 0 ;
+                } else if (computerPoints >= 5) {
+                    alert("The computer wins, better luck next time!");
+                    playerPoints = 0;
+                    computerPoints = 0 ;
+                }
+
             }
-        }
-    }
-    game();
+        //         console.log( "The winner is: " + winner);
+        //         console.log("Points: Player = " + playerPoints +
+        //          ", Computer: " + computerPoints);
+        //
